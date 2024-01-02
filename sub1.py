0a9c86f3c91bf731950bac2290234d5931cafc73
Sanket41998 <sanketwaikar5@gmail.com>
2024-01-02 12:59:37 +0530

    this is my second commit

--- sub1.py
+++ sub1.py
@@ -1,2 +1,13 @@
 a="this is just for checking that the second brach is working or not."
 print(a)
+
+class a:
+    def name(self):
+        print("sanket")
+class b(a):
+    def last_name(self):
+        print("waikar")
+obj1=b()
+obj2=a()
+obj1.last_name()
+obj1.name()
